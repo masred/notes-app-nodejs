e09d65ad03e9c7d8558eec0154fb58933f7f8c9f
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2023-06-11 21:49:19 +0700

    🎨 Restructure with hapi plugin

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -1,6 +1,12 @@
 module.exports = class NoteHandler {
   constructor(service) {
     this.service = service;
+
+    this.postNoteHandler = this.postNoteHandler.bind(this);
+    this.getNotesHandler = this.getNotesHandler.bind(this);
+    this.getNoteByIdHandler = this.getNoteByIdHandler.bind(this);
+    this.putNoteByIdHandler = this.putNoteByIdHandler.bind(this);
+    this.deleteNoteByIdHandler = this.deleteNoteByIdHandler.bind(this);
   }
 
   postNoteHandler(request, h) {
